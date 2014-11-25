6672116a543f7294c37c63c8338f6bf8bceda14a
Joe Hudson <joehud@gmail.com>
2014-11-25 13:43:16 -0500

    add unit tests

--- test/test.js
+++ test/test.js
@@ -1,17 +1,18 @@
 var sinon = require('sinon'),
-    chai = require('chai'),
-    sinonChai = require('sinon-chai'),
-    expect = chai.expect,
-    React = require('react'),
-    _ = require('underscore'),
-    $on = sinon.spy(),
-    $off = sinon.spy(),
-    $ = sinon.spy(function() {
-      return {
-        on: $on,
-        off: $off
-      };
-    });
+  chai = require('chai'),
+  sinonChai = require('sinon-chai'),
+  expect = chai.expect,
+  React = require('react'),
+  Backbone = require('backbone'), // not a dependency - only used as an events impl
+  _ = require('underscore'),
+  $on = sinon.spy(),
+  $off = sinon.spy(),
+  $ = sinon.spy(function() {
+    return {
+      on: $on,
+      off: $off
+    };
+  });
 chai.use(sinonChai);
 
 global.window = global.window || {
@@ -25,7 +26,7 @@ require('../react-events')(React, $);
 
 function newComponent(attributes, mixins) {
 
-  mixins = mixins || React.mixins.get('events');
+  mixins = mixins ? React.mixins.get(mixins) : React.mixins.get('events');
 
   var obj = {
     getDOMNode: sinon.spy(),
@@ -40,10 +41,12 @@ function newComponent(attributes, mixins) {
       this.trigger('componentDidUnmount');
     },
 
-    isMounted: function() { return this._mounted; },
+    isMounted: function() {
+      return this._mounted;
+    },
     trigger: function(method) {
       var rtn = [];
-      for (var i=0; i<mixins.length; i++) {
+      for (var i = 0; i < mixins.length; i++) {
         var func = mixins[i][method];
         if (func) {
           rtn.push(func.apply(this, Array.prototype.slice.call(arguments, 1)));
@@ -60,7 +63,7 @@ function newComponent(attributes, mixins) {
 
   var state, aggregateState;
 
-  for (var i=0; i<mixins.length; i++) {
+  for (var i = 0; i < mixins.length; i++) {
     var mixin = mixins[i];
     _.defaults(obj, mixin);
     state = mixin.getInitialState && mixin.getInitialState.call(obj);
@@ -107,6 +110,7 @@ describe('window events', function() {
 
 
 describe('ref events', function() {
+  it('should on and off ref events', function() {
     var eventSpy = function() {
       return {
         on: sinon.spy(),
@@ -132,23 +136,67 @@ describe('ref events', function() {
       }
     });
 
-    it('should on and off ref events', function() {
-      obj.mount();
-      expect(obj.refs.foo.on.callCount).to.eql(1);
-      expect(obj.refs.foo.on).to.have.been.calledWith('foo-event');
-      expect(obj.refs.bar.on.callCount).to.eql(1);
-      expect(obj.refs.bar.on).to.have.been.calledWith('bar-event');
+    obj.mount();
+    expect(obj.refs.foo.on.callCount).to.eql(1);
+    expect(obj.refs.foo.on).to.have.been.calledWith('foo-event');
+    expect(obj.refs.bar.on.callCount).to.eql(1);
+    expect(obj.refs.bar.on).to.have.been.calledWith('bar-event');
 
-      obj.refs.foo.on.getCall(0).args[1]('foo');
-      expect(obj.onFooEvent.callCount).to.eql(1);
-      expect(obj.onFooEvent).to.have.been.calledWith('foo');
-      expect(obj.onFooEvent.thisValues[0]).to.eql(obj);
+    obj.refs.foo.on.getCall(0).args[1]('foo');
+    expect(obj.onFooEvent.callCount).to.eql(1);
+    expect(obj.onFooEvent).to.have.been.calledWith('foo');
+    expect(obj.onFooEvent.thisValues[0]).to.eql(obj);
 
-      obj.refs.bar.on.getCall(0).args[1]('bar');
-      expect(obj.onBarEvent.callCount).to.eql(1);
-      expect(obj.onBarEvent).to.have.been.calledWith('bar');
-      expect(obj.onBarEvent.thisValues[0]).to.eql(obj);
+    obj.refs.bar.on.getCall(0).args[1]('bar');
+    expect(obj.onBarEvent.callCount).to.eql(1);
+    expect(obj.onBarEvent).to.have.been.calledWith('bar');
+    expect(obj.onBarEvent.thisValues[0]).to.eql(obj);
+  });
+});
+
+describe('prop events', function() {
+  it('should on and off prop events', function() {
+    var eventSpy = function() {
+      return {
+        on: sinon.spy(),
+        off: sinon.spy()
+      };
+    };
+    var obj = newComponent({
+      events: {
+        'prop:foo:foo-event': 'onFooEvent',
+        'prop:bar:bar-event': 'onBarEvent'
+      },
+      onFooEvent: sinon.spy(),
+      onBarEvent: sinon.spy(),
+      props: {
+        foo: {
+          on: sinon.spy(),
+          off: sinon.spy()
+        },
+        bar: {
+          on: sinon.spy(),
+          off: sinon.spy()
+        }
+      }
     });
+
+    obj.mount();
+    expect(obj.props.foo.on.callCount).to.eql(1);
+    expect(obj.props.foo.on).to.have.been.calledWith('foo-event');
+    expect(obj.props.bar.on.callCount).to.eql(1);
+    expect(obj.props.bar.on).to.have.been.calledWith('bar-event');
+
+    obj.props.foo.on.getCall(0).args[1]('foo');
+    expect(obj.onFooEvent.callCount).to.eql(1);
+    expect(obj.onFooEvent).to.have.been.calledWith('foo');
+    expect(obj.onFooEvent.thisValues[0]).to.eql(obj);
+
+    obj.props.bar.on.getCall(0).args[1]('bar');
+    expect(obj.onBarEvent.callCount).to.eql(1);
+    expect(obj.onBarEvent).to.have.been.calledWith('bar');
+    expect(obj.onBarEvent.thisValues[0]).to.eql(obj);
+  });
 });
 
 describe('custom event bindings', function() {
@@ -186,7 +234,7 @@ describe('custom event bindings', function() {
   it('custom methods (on/off) and static target', function() {
     var _handler = handler;
     React.events.handle('custom2', {
-      target:  _handler,
+      target: _handler,
       onKey: 'onCustom',
       offKey: 'offCustom'
     });
@@ -208,7 +256,7 @@ describe('custom event bindings', function() {
   it('should not provide any arguments if the handler method is "forceUpdate"', function() {
     var _handler = handler;
     React.events.handle('custom3', {
-      target:  _handler
+      target: _handler
     });
 
     var obj = newComponent({
@@ -224,7 +272,7 @@ describe('custom event bindings', function() {
   it('should handle regular expression handlers', function() {
     var _handler = handler;
     React.events.handle(/custom-.*/, {
-      target:  _handler,
+      target: _handler,
       onKey: 'onCustom',
       offKey: 'offCustom'
     });
@@ -241,5 +289,79 @@ describe('custom event bindings', function() {
 
     obj.unmount();
     expect(_handler.offCustom.callCount).to.eql(1);
+  });
+});
+
+describe('listen', function() {
+  it('should start listening to a target', function() {
+    var model = new Backbone.Model(),
+      obj = newComponent({
+        props: {
+          model: model
+        }
+      }, ['listen']),
+      spy = sinon.spy();
+
+    obj.listenTo(model, 'foo', spy);
+    model.trigger('foo');
+    // we shouldn't bind yet because we aren't mounted
+    expect(spy.callCount).to.eql(0);
+
+    obj.mount();
+    model.trigger('foo');
+    expect(spy.callCount).to.eql(1);
+
+    // we shouldn't bind now because we will be unmounted
+    obj.unmount();
+    model.trigger('foo');
+    expect(spy.callCount).to.eql(1);
+
+    // mount again and ensure that we rebind
+    obj.mount();
+    model.trigger('foo');
+    expect(spy.callCount).to.eql(2);
+    obj.unmount();
+    model.trigger('foo');
+    expect(spy.callCount).to.eql(2);
+  });
+
+  it('should listen to a target once', function() {
+    var model = new Backbone.Model(),
+      obj = newComponent({
+        props: {
+          model: model
+        }
+      }, ['listen']),
+      spy = sinon.spy();
+
+    obj.listenToOnce(model, 'foo', spy);
+    model.trigger('foo');
+    // we shouldn't bind yet because we aren't mounted
+    expect(spy.callCount).to.eql(0);
+
+    obj.mount();
+    model.trigger('foo');
+    expect(spy.callCount).to.eql(1);
+
+    model.trigger('foo');
+    expect(spy.callCount).to.eql(1);
+  });
+
+  it('should stop listening to a target', function() {
+    var model = new Backbone.Model(),
+      obj = newComponent({
+        props: {
+          model: model
+        }
+      }, ['listen']),
+      spy = sinon.spy();
+    obj.listenTo(model, 'foo', spy);
+    obj.mount();
+    model.trigger('foo');
+    expect(spy.callCount).to.eql(1);
+
+    obj.stopListening(model, 'foo', spy);
+    model.trigger('foo');
+    expect(spy.callCount).to.eql(1);
   });
 });
